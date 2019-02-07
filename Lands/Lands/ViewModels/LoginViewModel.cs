8d6bbc4711169992536bb0f2b19870b81dd9e58c
7abianAlvarez <46414014+7abianAlvarez@users.noreply.github.com>
2019-02-07 14:35:27 -0600

    Login View Model

--- Lands/Lands/ViewModels/LoginViewModel.cs
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -1,28 +1,137 @@
-﻿namespace Lands.ViewModels
+﻿using GalaSoft.MvvmLight.Command;
+namespace Lands.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.Text;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
-    class LoginViewModel
+    public class LoginViewModel : INotifyPropertyChanged
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Attributes
+        private string password;
+        private bool isRunning;
+        public bool isEnable;
+        #endregion
+
         #region Properties
         public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsRunning { get; set; }
+        public string Password
+        {
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                if(this.password != value)
+                {
+                    this.password = value;
+                    PropertyChanged?.Invoke(
+                        this, new PropertyChangedEventArgs(nameof(this.Password)));
+                }
+            }
+        }
+        public bool IsRunning
+        {
+            get
+            {
+                return this.isRunning;
+            }
+            set
+            {
+                if (this.isRunning != value)
+                {
+                    this.isRunning = value;
+                    PropertyChanged?.Invoke(
+                        this, new PropertyChangedEventArgs(nameof(this.IsRunning)));
+                }
+            }
+        }
         public bool IsRemembered { get; set; }
+        public bool IsEnable
+        {
+            get
+            {
+                return this.isEnable;
+            }
+            set
+            {
+                if (this.isEnable != value)
+                {
+                    this.isEnable = value;
+                    PropertyChanged?.Invoke(
+                        this, new PropertyChangedEventArgs(nameof(this.IsEnable)));
+                }
+            }
+        }
         #endregion
 
         #region Constructors
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnable = true; ;
         }
         #endregion
 
         #region Commands
-        public ICommand LoginCommand { get; set; }
+        public ICommand LoginCommand { get { return new RelayCommand(Login); } }
+
+        
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error not found",
+                    "You must enter an email.",
+                    "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a password.",
+                    "Accept");
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnable = false;
+
+            if (this.Email != "7abiana@gmail" || this.Password != "a")
+            {
+                this.IsRunning = false;
+                this.IsEnable = true;
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password incorrect",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnable = true;
+
+            await Application.Current.MainPage.DisplayAlert(
+                "Ok",
+                "Conection succesfull",
+                "Accept");
+            return;
+            
+        }
         #endregion
     }
 }
